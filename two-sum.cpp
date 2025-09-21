10293292a15269a5b95c532c8cdcf2cf22e28102
emmanueluc322 <61635797+emmanueluc322@users.noreply.github.com>
2025-09-20 22:20:55 -0400

    first problem solvem

---
+++ two-sum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> sum; 
+        for(int i=0; i<nums.size(); ++i){
+            int offset = target - nums[i];
+
+            if(sum.find(offset) != sum.end()){
+                //cout<<"target: "<<target<<"= "<<sum[offset] <<", "<< i<<endl;
+                return {sum[offset], i};
+            }else{
+                    sum[nums[i]] = i;
+
+            }
+        }
+
+        return{};
+    }
+};
+
+
+int main(){
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+
+    Solution sol;
+    vector<int> result = sol.twoSum(nums, target);
+    cout<<"target is at index: "<<result[0]<<", "<<result[1]<<endl;
+
+}
